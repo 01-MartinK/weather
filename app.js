baf50b7ac4fd9267aa7c8d941e051d82d6b4161f
Martin K <thedeadgamer101@gmail.com>
2021-10-20 11:31:31 +0300

    Broken api key

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-function myFunction() {
-  const inpObj = document.getElementById("id1");
-  if (!inpObj.checkValidity()) {
-    document.getElementById("demo").innerHTML = inpObj.validationMessage;
-  } else {
-    document.getElementById("demo").innerHTML = "Input OK";
-  } 
-} 
+function weatherBalloon( cityID ) {
+  var key = '{641613077d3def880b4b69b47b3e8c28}';
+  fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
+  .then(function(resp) { return resp.json() }) // Convert data to json
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function() {
+    // catch any errors
+  });
+}
+
+window.onload = function() {
+  weatherBalloon( 6167865 );
+}
+
+function drawWeather( d ) {
+	var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+	
+	document.getElementById('description').innerHTML = d.weather[0].description;
+	document.getElementById('temp').innerHTML = celcius + '&deg;';
+	document.getElementById('location').innerHTML = d.name;
+}
